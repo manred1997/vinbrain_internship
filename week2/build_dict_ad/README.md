dca506f313a2740a2b6e407961eb334fa814f12c
Trần Hoàng Vũ (VNCDLL-CTÐTKSAI) <vuth5@vingroup.net>
2021-03-20 16:58:35 +0700

    update result

--- week2/build_dict_ad/README.md
+++ week2/build_dict_ad/README.md
@@ -11,5 +11,5 @@
 - list_long_form* is file contains list of words that can be abbreviated
 - And json file contains acronym and expansion respectively
 - Histogram of words that can be abbreviated
-![](histogram_eng_arconym.png)
+![](./resul eng/thistogram_eng_arconym.png)
 
--- week2/build_dict_ad/build_dict.py
+++ week2/build_dict_ad/build_dict.py
@@ -40,10 +40,10 @@ def POS(string):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_file", type=str, default="./cxrv2.txt", help="Input file")
-    parser.add_argument("--output_file", type=str, default="./list_long_form_cxrv2.txt", help="Output file")
-    parser.add_argument("--dict_arc", type=str, default="./dict_acronym_cxrv2.json", help="Json file")
-    parser.add_argument("--top_k", type=int, default=1000, help="Top k words that can be abbreviated")
+    parser.add_argument("--input_file", type=str, default="./english_clean.txt", help="Input file")
+    parser.add_argument("--output_file", type=str, default="./list_long_form.txt", help="Output file")
+    parser.add_argument("--dict_arc", type=str, default="./dict_acronym.json", help="Json file")
+    parser.add_argument("--top_k", type=int, default=40, help="Top k words that can be abbreviated")
     args = parser.parse_args()
 
     with open(args.input_file, 'r') as f:
@@ -87,25 +87,25 @@ if __name__ == "__main__":
     plt.xticks(np.linspace(1,len(sorted_unique), 10, dtype=int))
     plt.ylabel("Number of words")
     plt.title("Histogram of words that can be abbreviated")
-    plt.savefig("histogram_eng_arconym.png")
+    plt.savefig("histogram_eng_cxrv_arconym.png")
     plt.show()
 
-    # top_k_words = []
-    # for i in range(args.top_k):
-    #     top_k_words.append(sorted_unique[i]["unique"])
+    top_k_words = []
+    for i in range(args.top_k):
+        top_k_words.append(sorted_unique[i]["unique"])
 
-    # with open(args.output_file, "w") as f:
-    #     f.write("\n".join(top_k_words))
-    # dict_file = {}
-    # for i, value in enumerate(top_k_words):
-    #     if len(value.split(" ")) == 1 : dict_file[f"None_{i}"] = value
-    #     else:
-    #         token = value.split(" ")
-    #         acronym = "".join([j[0] for j in token])
-    #         dict_file[acronym] = value
-    # # print(dict_file)
-    # with open(args.dict_arc, "w") as f:
-    #     json.dump(dict_file, f)
-    with open("tmp.txt") as f:
-        for item in sorted_unique:
-            f.write("%s\n" % item)
+    with open(args.output_file, "w") as f:
+        f.write("\n".join(top_k_words))
+    dict_file = {}
+    for i, value in enumerate(top_k_words):
+        if len(value.split(" ")) == 1 : dict_file[f"None_{i}"] = value
+        else:
+            token = value.split(" ")
+            acronym = "".join([j[0] for j in token])
+            dict_file[acronym] = value
+    # print(dict_file)
+    with open(args.dict_arc, "w") as f:
+        json.dump(dict_file, f)
+    # with open("tmp.txt") as f:
+    #     for item in sorted_unique:
+    #         f.write("%s\n" % item)
