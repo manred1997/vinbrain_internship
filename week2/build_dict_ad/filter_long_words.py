767e27cb4340a9f3dea673da22163eb87136306c
Trần Hoàng Vũ (VNCDLL-CTÐTKSAI) <vuth5@vingroup.net>
2021-03-23 17:54:12 +0700

    update result

--- week2/build_dict_ad/filter_long_words.py
+++ week2/build_dict_ad/filter_long_words.py
@@ -3,14 +3,18 @@ import re
 
 def remove_string(string):
     if re.search("(xx+|\sx\s|x\.|^x\s|\sx$)", string):  return True
+    elif re.search("^xx*\s", string): return True
     elif re.search("^\.\w", string):    return True
     else: return False
 
 def filter_long_words(data):
-    for key in data.items():
-        for value in data[key]:
-            if remove_string(value):
-                data[key].remove(value)
+    for i in range(20):
+        for key in data:
+            for value in data[key]:
+                if remove_string(value):
+                    # print(key)
+                    # print(value)
+                    data[key].remove(value)
     new_data = {}
     for key in data:
         if not data[key]: continue
@@ -22,5 +26,5 @@ with open("./result_cxr/long_dict.json", "r") as f:
 
 data = filter_long_words(data)
 
-with open("./result_cxr/filtered_long_dict.json") as f:
+with open("./result_cxr/filtered_long_dict.json", "w") as f:
     json.dump(data, f)
