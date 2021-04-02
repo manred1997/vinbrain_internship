cbde3f1d18a336c7c691241922cc0b07f5fbef77
Trần Hoàng Vũ (VNCDLL-CTÐTKSAI) <vuth5@vingroup.net>
2021-04-02 11:47:24 +0700

    week4

---
+++ vi_preprocessing.py
@@ -0,0 +1,65 @@
+import argparse
+import os
+import json
+
+import numpy as np
+
+def normalize(list_token: list):
+    """
+    Function: Lower string
+    """
+    return list(map(lambda x: x.lower(), list_token))
+
+def preprocessing(data: list, label: str):
+    """
+    Function: Preprocessing data
+    args:
+        data: Raw data
+        label: pos or neg
+    """
+    for sample in data:
+        sample['text'] = sample['text'].lower()
+        if label == "pos": sample["label"] = 1
+        else: sample["label"] = 0
+    return data
+
+def create_inputs_targets(examples):
+    """
+    Function: Create dictionary dataset for training
+    """
+    dataset_dict = {
+        "input_ids": [],
+        "token_type_ids": [],
+        "attention_mask": [],
+        "start_token_idx": [],
+        "end_token_idx": [],
+        "label": []
+    }
+    for item in examples:
+        if item.skip is False:
+            for key in dataset_dict:
+                dataset_dict[key].append(getattr(item, key))
+        
+    X = [dataset_dict["input_ids"], dataset_dict["token_type_ids"], dataset_dict["attention_mask"]]
+    Y = [dataset_dict["start_token_idx"], dataset_dict["end_token_idx"], dataset_dict["label"]]
+    return X, Y
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", type=str, default="../AAAI-21-SDU-shared-task-2-AD/dataset/train.json", 
+                        help= "Dataset for Acronym Disambiguation")
+    parser.add_argument("--data_folder", type=str, default="./pos_data",
+                        help= "Folder for sampling positive dataset")
+    parser.add_argument("--mode", type=str, default="train",
+                        help= "Mode of dataset")
+    args = parser.parse_args()
+
+    with open(args.data, "r", encoding="UTF-8") as f:
+        data = json.load(f)
+    
+    if not os.path.isdir(args.data_folder): os.mkdir(args.data_folder)
+
+    data_pos = preprocessing(data, "pos")
+
+    with open(os.path.join(args.data_folder, f"{args.mode}_pos_data.json"), "w", encoding="UTF-8") as f:
+        json.dump(data_pos, f)
---
+++ vi_sample.py
@@ -0,0 +1,89 @@
+from tqdm import tqdm
+import sys
+from colorama import Fore
+
+class Sample:
+    def __init__(self, tokenizer, expansion, context, start_char_idx, len_acronym, label, max_seq_lenght=384):
+        self.tokenizer = tokenizer #tokenizer BertWordPieceTokenizer
+        self.expansion = expansion
+        self.context = context
+        self.start_char_idx = start_char_idx
+        self.len_acronym = len_acronym
+        # self.id = ids
+        self.max_seq_lenght = max_seq_lenght
+        self.skip = False
+        
+        self.start_token_idx = -1
+        self.end_token_idx = -1
+
+        self.label = int(label)
+        
+    def preprocess(self):
+        tokenized_expansion = self.tokenizer.encode(self.expansion)
+        tokenized_context = self.tokenizer.encode(self.context)
+        
+        end_char_idx = self.start_char_idx + self.len_acronym
+        if end_char_idx > len(self.context): 
+            self.skip = True
+            return
+        
+        is_char_in_context = [0]*len(self.context)
+        for idx in range(self.start_char_idx, end_char_idx):
+            is_char_in_context[idx] = 1
+        
+        arc_token_idx  = []
+        for idx, (start, end) in enumerate(tokenized_context.offsets):
+            if sum(is_char_in_context[start:end]) > 0: arc_token_idx.append(idx)
+        if len(arc_token_idx) == 0:
+            self.skip = True
+            return
+        self.start_token_idx = arc_token_idx[0]
+        self.end_token_idx = arc_token_idx[-1]
+
+        # ###################### START AND END TOKEN #############################
+        # tokenized_context_ids = tokenized_context.ids
+        # tokenized_context_ids.insert(self.start_token_idx, 30522)
+        # tokenized_context_ids.insert(self.end_token_idx+2, 30523)
+
+        # self.start_token_idx += 1
+        # self.end_token_idx += 1
+        
+        # ######################################################################
+
+        input_ids = tokenized_context.ids + tokenized_expansion.ids[1:]
+        token_type_ids = [0] * len(tokenized_context.ids) + [1] * len(tokenized_expansion.ids[1:])
+        attention_mask = [1] * len(input_ids)
+        
+        
+        padding_length = self.max_seq_lenght - len(input_ids)
+        if padding_length > 0:
+            input_ids = input_ids + ([0]* padding_length)
+            token_type_ids = token_type_ids + ([0]* padding_length)
+            attention_mask = attention_mask + ([0]* padding_length)
+        elif padding_length < 0:
+            self.skip = True
+            return
+        
+        self.input_ids = input_ids
+        self.token_type_ids = token_type_ids
+        self.attention_mask = attention_mask
+
+
+def create_examples(raw_data, desc, tokenizer):
+    p_bar = tqdm(total=len(raw_data), desc=desc,
+                 position=0, leave=True,
+                 file=sys.stdout, bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.BLUE, Fore.RESET))
+    examples = []
+    for item in raw_data:
+        expansion = item["expansion"]
+        context = item["text"]
+        start_char_idx = item["start_char_idx"]
+        lenght_acronym = item["lenght_acronym"]
+        label = item["label"]
+        # ids  = item["id"]
+        example = Sample(tokenizer, expansion, context, start_char_idx, lenght_acronym, label)
+        example.preprocess()
+        examples.append(example)
+        p_bar.update(1)
+    p_bar.close()
+    return examples
